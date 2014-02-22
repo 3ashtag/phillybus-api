cffe89fc6b006851c4d39960491b23beb56a6c24
Timothy Hahn <timyhahn@gmail.com>
2014-02-22 15:09:45 -0500

    Added router API endpoint

--- src/main/scala/phillybus/Server.scala
+++ src/main/scala/phillybus/Server.scala
@@ -3,6 +3,7 @@ package phillybus
 import akka.actor.ActorSystem
 import akka.actor.Props
 
+import org.mashupbots.socko.events.HttpResponseStatus
 import org.mashupbots.socko.routes._
 import org.mashupbots.socko.webserver.{WebServer, WebServerConfig}
 import org.slf4j.{Logger, LoggerFactory}
@@ -21,11 +22,20 @@ class HelloHandler extends Actor {
 }
 
 object Server extends App {
+  object LatitudeQueryString extends QueryStringField("lat")
+  object LongitudeQueryString extends QueryStringField("long")
+
   private val log = LoggerFactory.getLogger(getClass)
   val actorSystem = ActorSystem("PhillyBusFinderSystem")
   val routes = Routes({
-    case GET(request) => {
-      actorSystem.actorOf(Props[HelloHandler]) ! request
+    case HttpRequest(request) => request match {
+      case (GET(Path("/arrivals/nearby")) & LatitudeQueryString(latitude) & LongitudeQueryString(longitude)) => {
+        actorSystem.actorOf(Props[HelloHandler]) ! request
+      }
+      case _ => {
+        println(request.endPoint)
+        request.response.write(HttpResponseStatus.BAD_REQUEST)
+      }
     }
   })
 
