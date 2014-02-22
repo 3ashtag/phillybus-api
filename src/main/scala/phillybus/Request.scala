e5610eab9de08cb1dccf1b130849001a92561320
jarrad <jmbbat@gmail.com>
2014-02-22 16:04:04 -0500

    added request for transitview by route

--- src/main/scala/phillybus/Request.scala
+++ src/main/scala/phillybus/Request.scala
@@ -1,22 +1,32 @@
 package phillybus
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import scalaj.http._
+
+import akka.pattern.ask
+import akka.util.Timeout
 import akka.actor.Actor
 import akka.actor.Props
 import akka.event.Logging
 import akka.actor.ActorSystem
-import scala.collection.mutable.HashMap
+
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
 class Request() extends Actor {
 	val log = Logging(context.system, this)
 
-	def getRequest(get : GetRequest): String = {
-		val request = Http(get.url).asString
+	def getRequest(get : GetRequest) = get match {
+    case GetRequest(t : String, m : Map[String, String]) =>
+		sender ! Http(get.url).header("content-type", "application/json").params(get.params).option(HttpOptions.readTimeout(5000)).asString
+    case GetRequest(t : String, null) =>
+      sender ! Http(get.url).header("content-type", "application/json").option(HttpOptions.readTimeout(5000)).asString
+    case _ => log.warning("Not a valid request")
 	}
 
-	def postRequest(postRequest : PostRequest) : String = {
-		"test"
-
+	def postRequest(post : PostRequest) {
+		sender ! Http.post(post.url).params(post.params).asString
 	}
 
 	def receive = {
@@ -26,11 +36,36 @@ class Request() extends Actor {
 	}
 }
 
+
+
 object RequestApp extends App {
-	val actorSystem = ActorSystem("requestSystem")
-	val myActor = actorSystem.actorOf(Props[Request], name="requestActor")
+  implicit val timeout = Timeout(10 seconds)
+	implicit val formats = DefaultFormats
+  val actorSystem = ActorSystem("requestSystem")
+  println(getTransitForRoute(20))	
+
+  def getTransitForRoute(routeId : Int) : JSONSepta = {
+    val myActor = actorSystem.actorOf(Props[Request], name="requestActor")
+    val future = myActor ? GetRequest("http://www3.septa.org/hackathon/TransitView", Map("route" -> routeId.toString))
+    val result = Await.result(future, timeout.duration).asInstanceOf[String]
+    //  println(result)
+    try {
+      val json = parse(result)
+      json.extract[JSONSepta]
+    } catch {
+      case _ => println("The routeId is not valid")
+      null
+    }
+  } 
+
 }
 
-case class GetRequest(url : String, header : String, params : HashMap[String, String]) {}
+case class GetRequest(url : String, params : Map[String, String] = null) {}
 
-case class PostRequest(url : String, params : HashMap[String, String]) 
+case class PostRequest(url : String, params : Map[String, String]) 
+
+case class JSONBus(lat : Either[String, Double], lng : String, label : String, VehicleID : String,
+			 BlockID : String, Direction : String, destination : String, Offset : String)
+case class JSONSepta(bus : List[JSONBus])
+case class JSONRoute(route : Map[Int, List[JSONBus]])
+case class JSONTransitAll(data : List[JSONRoute])
